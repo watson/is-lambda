e1a8869bcb175b563acabc15f916571878f2508e
Clement Kerneur <clement.dev@gmail.com>
2017-05-23 07:35:13 +0400

    Test with 2 environment variables

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ var assert = require('assert')
 var clearRequire = require('clear-require')
 
 process.env.AWS_EXECUTION_ENV = 'AWS_Lambda_nodejs6.10'
+process.env.LAMBDA_TASK_ROOT = '/var/task'
 
 var isCI = require('./')
 assert(isCI)
