a33d4007ba3063f6fce63676c3dac7968c9af39d
Clement Kerneur <ClementKerneur@users.noreply.github.com>
2017-05-24 23:01:53 +0400

    Return false when run via ServerLess Framework (#1)

--- test.js
+++ test.js
@@ -3,12 +3,13 @@
 var assert = require('assert')
 var clearRequire = require('clear-require')
 
+process.env.AWS_EXECUTION_ENV = 'AWS_Lambda_nodejs6.10'
 process.env.LAMBDA_TASK_ROOT = '/var/task'
 
 var isCI = require('./')
 assert(isCI)
 
-delete process.env.LAMBDA_TASK_ROOT
+delete process.env.AWS_EXECUTION_ENV
 
 clearRequire('./')
 isCI = require('./')
